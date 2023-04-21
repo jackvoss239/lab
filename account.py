657572433cba932e7b7794d53cc035873c76ac08
jackvoss239 <tsm.voss@gmail.com>
2023-04-21 16:46:45 -0500

    Added docstrings and type hinting

--- account.py
+++ account.py
@@ -1,24 +1,48 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name:str) => None:
+        """
+        Function to set up object
+        :param name: Account name
+        """
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount:float) => bool:
+        """
+        Function to add money to account
+        :param amount: Amount of money deposited
+        :return: Returns True if transaction was successful
+        :return: Returns False if Transaction was unsuccssful
+        """
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount:float) => bool:
+        """
+        Function to withdraw money from account
+        :param amount: Amount of money withdrawn
+        :return: Returns True if transaction was successful
+        :return: Returns False if Transaction was unsuccssful
+        """
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) => float:
+        """
+        Function to return account balance
+        :return: Returns account balance
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) => str:
+        """
+        Function to return account name
+        :return: Returns account name
+        """
         return self.__account_name
